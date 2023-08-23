fcdf34d122416eb04f205965d0673003e0d32463
Rahil Nelliyali <124560240+Rahil-Nelliyali@users.noreply.github.com>
2023-08-23 20:01:54 +0530

    Update socketprovider.jsx

--- src/components/socket/socketprovider.jsx
+++ src/components/socket/socketprovider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext, useMemo, useEffect } from "react";
 
 const SocketContext = createContext(null);
 
@@ -9,29 +9,17 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({ children }) => {
-    // Create a WebSocket connection using the native browser API
     const socket = useMemo(() => {
         const wsProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-        const socketURL = `${wsProtocol}//${window.location.host}/ws/chat/`;
+        const socketURL = `${wsProtocol}//drivenowbackend.onrender.com/ws/chat/`; // Update with your actual backend's WebSocket endpoint
 
-        return new WebSocket(socketURL);
+        try {
+            return new WebSocket(socketURL);
+        } catch (error) {
+            console.error("WebSocket connection failed:", error);
+            return null; // Return null in case of connection failure
+        }
     }, []);
-
-    socket.onopen = (event) => {
-        console.log('WebSocket connection opened:', event);
-    };
-
-    socket.onmessage = (event) => {
-        console.log('WebSocket message received:', event);
-    };
-
-    socket.onclose = (event) => {
-        console.log('WebSocket connection closed:', event);
-    };
-
-    socket.onerror = (error) => {
-        console.error('WebSocket error:', error);
-    };
 
     return (
         <SocketContext.Provider value={socket}>
